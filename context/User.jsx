652185b110d587e3ddc1a868615be3bbca9764b8
Waldo <wilfdavidjames@gmail.com>
2023-11-08 15:41:43 +0000

    made user data persist between refreshes

--- context/User.jsx
+++ context/User.jsx
@@ -1,10 +1,21 @@
-import { createContext,useState } from "react";
+import { createContext,useEffect,useState } from "react";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({children})=>{
 
-    const [user, setUser]=useState({})
+    const [user, setUser]=useState(()=>{
+        const stickyUser = window.localStorage.getItem('userData');
+        if(stickyUser === null){
+            return 'hello'
+        }else{
+            return JSON.parse(stickyUser)
+        }
+    })
+
+    useEffect(()=>{
+        window.localStorage.setItem('userData',JSON.stringify(user))
+    },[user])
 
     return(
         <UserContext.Provider value={{user, setUser}}>
