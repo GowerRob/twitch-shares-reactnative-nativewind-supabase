fdf99231ee4ca95b7da3d43e9e09b69aea526b11
Howling Techie <howling.techie@gmail.com>
2023-11-09 11:52:30 +0000

    More work on game preview and transaction components, with better scaling for small screens.

--- components/Transactions.jsx
+++ components/Transactions.jsx
@@ -16,11 +16,6 @@ const Transactions = ({data}) => {
     const {total_shares_owned, total_shares_value, transactions} = data;
     const colorScheme = "dark";
 
-    const styles = {
-        tableHeader: `font-bold text-text-${colorScheme} text-right flex-1`,
-        tableData: `text-text-${colorScheme} text-right flex-1`,
-    };
-
     return (
         <View className={`rounded-lg p-4 m-4 bg-background-${colorScheme}`}>
             <Text className={`text-xl font-bold text-text-dark mb-4`}>Transaction History</Text>
@@ -31,19 +26,30 @@ const Transactions = ({data}) => {
             <View className={`border border-text-dark rounded-lg`}>
                 <View
                     className={`flex flex-row justify-between border-b border-dar border-text-${colorScheme} py-2 px-4`}>
-                    <Text className={styles.tableHeader}>Date</Text>
-                    <Text className={styles.tableHeader}>Quantity</Text>
-                    <Text className={styles.tableHeader}>Value</Text>
-                    <Text className={styles.tableHeader}>New Total</Text>
+                    <Text className={`font-bold text-text-${colorScheme} text-right w-48`}>Date</Text>
+                    {transactions[0].game &&
+                        <Text className={`font-bold text-text-${colorScheme} text-right flex-1`}>Game</Text>}
+                    <Text
+                        className={`font-bold text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>Quantity</Text>
+                    <Text
+                        className={`font-bold text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>Value</Text>
+                    {/*<Text*/}
+                    {/*    className={`font-bold text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>New*/}
+                    {/*    Total</Text>*/}
                 </View>
                 {transactions.map((transaction, index) => (
                     <View className={`flex flex-row justify-between border-b border-${colorScheme}-300 py-2 px-4`}
                           key={index}>
                         <Text
-                            className={styles.tableData}>{transaction.date.toDateString() + " " + transaction.date.toLocaleTimeString()}</Text>
-                        <Text className={styles.tableData}>{transaction.quantity}</Text>
-                        <Text className={styles.tableData}>{numberWithCommas(transaction.value)}</Text>
-                        <Text className={styles.tableData}>{transaction.new_total}</Text>
+                            className={`text-text-${colorScheme} text-right w-48`}>{transaction.date.toDateString() + " " + transaction.date.toLocaleTimeString()}</Text>
+                        {transactions[0].game &&
+                            <Text className={`text-text-${colorScheme} text-right flex-1`}>{transaction.game}</Text>}
+                        <Text
+                            className={`text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>{transaction.quantity}</Text>
+                        <Text
+                            className={`text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>{numberWithCommas(transaction.value)}</Text>
+                        {/*<Text*/}
+                        {/*    className={`text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>{transaction.new_total}</Text>*/}
                     </View>
                 ))}
             </View>
