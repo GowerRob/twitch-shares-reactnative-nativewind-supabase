9ff172f3901f5254b08eb90142535a18a2aea446
Howling Techie <howling.techie@gmail.com>
2023-11-13 09:06:19 +0000

    First draft of the home page component

--- app/index.js
+++ app/index.js
@@ -1,38 +1,38 @@
-import { View, Text, ScrollView, SafeAreaView } from "react-native";
-import { useState } from "react";
-import { Stack, useRouter } from "expo-router";
+import {View, Text, ScrollView, SafeAreaView} from "react-native";
+import {useState} from "react";
+import {Stack, useRouter} from "expo-router";
 import Login from "../components/Login";
 
 
 // The below snippet also multiple nativewind classes to be applied
-import { NativeWindStyleSheet } from "nativewind";
+import {NativeWindStyleSheet} from "nativewind";
+import HomeComponent from "../components/HomeComponent";
 
 NativeWindStyleSheet.setOutput({
-  default: "native",
+    default: "native",
 });
 
 const Home = () => {
 
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <SafeAreaView>
-      <Stack.Screen
-        options={{
-          headerStyle: { backgroundColor: "white" },
-          headerShadowVisible: false,
-          headerTitle: "",
-        }}
-      />
+    return (
+        <SafeAreaView className="flex-1 h-screen bg-black">
+            <Stack.Screen
+                options={{
+                    headerStyle: {backgroundColor: "white"},
+                    headerShadowVisible: false,
+                    headerTitle: "",
+                }}
+            />
 
-      <ScrollView>
-        <View className="flex-1 ">
-          <Login />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+            <ScrollView className="flex-1 h-screen">
+                <View className="flex-1 h-screen">
+                    <Login/>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default Home;
-
