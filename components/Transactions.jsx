3df07fc9957a096f705e9e43ca42ffa3932253ce
Howling Techie <howling.techie@gmail.com>
2023-11-14 09:47:17 +0000

    Added checks to see if transactions have been provided

--- components/Transactions.jsx
+++ components/Transactions.jsx
@@ -7,7 +7,7 @@ import {NativeWindStyleSheet} from "nativewind";
 
 NativeWindStyleSheet.setOutput({});
 
-function numberWithCommas(x) {
+function numberWithCommas(x = 0) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
@@ -15,7 +15,6 @@ const Transactions = ({data, show_overview = true}) => {
 
     const {total_shares_owned, total_shares_value, transactions} = data;
     const colorScheme = "dark";
-
     return (
         <View className={`rounded-lg p-4 m-4 bg-background-${colorScheme}`}>
             <Text className="text-2xl font-bold text-text-dark mb-4">Transaction History</Text>
@@ -27,15 +26,16 @@ const Transactions = ({data, show_overview = true}) => {
                 <View
                     className={`flex flex-row justify-between border-b border-dar border-text-${colorScheme} py-2 px-4`}>
                     <Text className={`font-bold text-text-${colorScheme} text-right w-48`}>Date</Text>
-                    {transactions[0].game &&
+                    {transactions.length > 0 && transactions[0].game &&
                      <Text className={`font-bold text-text-${colorScheme} text-right flex-1`}>Game</Text>}
-                    <Text
-                        className={`font-bold text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>Quantity</Text>
-                    <Text
-                        className={`font-bold text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>Value</Text>
+                    {transactions.length > 0 && <Text
+                        className={`font-bold text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>Quantity</Text>}
+                    {transactions.length > 0 && <Text
+                        className={`font-bold text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>Value</Text>}
                     {/*<Text*/}
                     {/*    className={`font-bold text-text-${colorScheme} text-right ${transactions[0].game ? "w-24" : "flex-1"}`}>New*/}
                     {/*    Total</Text>*/}
+
                 </View>
                 {transactions.map((transaction, index) => (
                     <View className={`flex flex-row justify-between border-b border-${colorScheme}-300 py-2 px-4`}
