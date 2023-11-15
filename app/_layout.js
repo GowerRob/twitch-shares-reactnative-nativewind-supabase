5e2e95499c5b58bd6b3dd7ea29577c53e6dcdaf0
Gundars Geida <gundar.geida@gmail.com>
2023-11-15 15:07:56 +0000

    as per

--- app/_layout.js
+++ app/_layout.js
@@ -1,26 +1,21 @@
-import {Slot} from 'expo-router'
-import Header from '../components/Header'
-import { UserProvider } from '../context/User'
-import Footer from '../components/Footer'
+import { Slot } from "expo-router";
+import Header from "../components/Header";
+import { UserProvider } from "../context/User";
+import Footer from "../components/Footer";
 
-import { View } from 'react-native'
+import { View } from "react-native";
 
 const Layout = () => {
   return (
     <>
-            <UserProvider>
-                <Header />
-                <View className={`py-20 h-full bg-background-dark`}>
-                    <Slot />
-                </View>
-                <Footer />
-            </UserProvider>
+      <UserProvider>
+        <Header />
+        <View className={`py-20 h-full bg-background-dark`}>
+          <Slot />
+        </View>
+        <Footer />
+      </UserProvider>
     </>
-
-
   );
-
-
 };
-
-
+export default Layout;
