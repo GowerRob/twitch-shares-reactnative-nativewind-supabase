318daa5110dc8085c068f38519ab5a3050d6c402
Howling Techie <howling.techie@gmail.com>
2023-11-15 13:57:09 +0000

    Updated portfolio history to show historical balance value.

--- components/PortfolioHistory.jsx
+++ components/PortfolioHistory.jsx
@@ -1,45 +1,70 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { NativeWindStyleSheet } from "nativewind";
+import {View, Text} from "react-native";
+import {NativeWindStyleSheet} from "nativewind";
 import {
-  VictoryArea,
-  VictoryAxis,
-  VictoryChart,
-  VictoryLine,
-  VictoryPie,
-  VictoryTheme,
+    VictoryArea,
+    VictoryAxis,
+    VictoryChart,
+    VictoryLine,
+    VictoryPie,
+    VictoryTheme, VictoryTooltip, VictoryVoronoiContainer,
 } from "victory-native";
 
 NativeWindStyleSheet.setOutput({});
 
 function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-const ShareOverview = ({ portfolio_history }) => {
-  const colorScheme = "dark";
-  return (
-    <View className={`rounded-lg p-4 m-4 bg-background-${colorScheme}`}>
-      <Text className="text-2xl font-bold text-text-dark mb-4">
-        Portfolio Value
-      </Text>
-      <View className="flex-grow m-0 p-0 rounded-lg hidden sm:flex">
-        <VictoryChart
-          height={300}
-          width={600}
-          theme={VictoryTheme.material}
-          padding={{ left: 100 }}
-        >
-          <VictoryAxis dependentAxis />
-          <VictoryLine
-            data={portfolio_history.map((value) => {
-              return { x: value.time, y: value.total_value };
-            })}
-          />
-        </VictoryChart>
-      </View>
-    </View>
-  );
+const ShareOverview = ({portfolio_history}) => {
+    const colorScheme = "dark";
+    return (
+        <View className={`rounded-lg p-4 m-4 bg-background-${colorScheme}`}>
+            <Text className="text-2xl font-bold text-text-dark mb-4">
+                Portfolio Value
+            </Text>
+            <View className="flex-grow m-0 p-0 rounded-lg hidden sm:flex">
+                <VictoryChart
+                    height={300}
+                    width={600}
+                    theme={VictoryTheme.material}
+                    padding={{left: 100}}
+                >
+                    <VictoryAxis dependentAxis/>
+                    <VictoryLine
+                        data={portfolio_history.map((value) => {
+                            return {x: value.time, y: value.total_value};
+                        })}
+                        style={{
+                            data: {
+                                stroke: "green",
+                            }
+                        }}
+                    />
+                    <VictoryLine
+                        data={portfolio_history.map((value) => {
+                            return {x: value.time, y: value.credits};
+                        })}
+                        style={{
+                            data: {
+                                stroke: "#3d84ce",
+                            }
+                        }}
+                    />
+                    <VictoryLine
+                        data={portfolio_history.map((value) => {
+                            return {x: value.time, y: value.credits + value.total_value};
+                        })}
+                        style={{
+                            data: {
+                                stroke: "grey",
+                            }
+                        }}
+                    />
+                </VictoryChart>
+            </View>
+        </View>
+    );
 };
 
 export default ShareOverview;
